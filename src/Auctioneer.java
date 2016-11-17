c60b4285353100d24d57ad23a697ff4fab43fc22
Araxi Mekhitarian <araxii@live.se>
2016-11-17 15:45:53 +0100

    Init

---
+++ src/Auctioneer.java
@@ -0,0 +1,5 @@
+/**
+ * Created by araxi on 2016-11-17.
+ */
+public class Auctioneer {
+}
---
+++ src/Bidder.java
@@ -0,0 +1,7 @@
+import jade.core.Agent;
+
+/**
+ * Created by araxi on 2016-11-17.
+ */
+public class Bidder extends Agent {
+}
