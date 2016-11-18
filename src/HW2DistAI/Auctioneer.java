8bac052986bf8108df995f6640578a67c1d6d01d
Araxi Mekhitarian <araxii@live.se>
2016-11-18 17:27:08 +0100

    Final

--- src/HW2DistAI/Auctioneer.java
+++ src/HW2DistAI/Auctioneer.java
@@ -2,38 +2,45 @@ package HW2DistAI;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
 
 /**
- * Created by araxi on 2016-11-17.
+ * Created by Araxi and Amir on 2016-11-17.
  */
 public class Auctioneer extends Agent {
-    private AID a1 = new AID("HW2DistAI.Auctioneer", AID.ISLOCALNAME);
+    private AID a1 = new AID("Auctioneer", AID.ISLOCALNAME);
 
     int startPrice;
     int bottomPrice;
     AID [] bidderAgents;
+    ACLMessage receivedMsg;
+    int proposeCount = 0;
 
     @Override
     protected void setup() {
-        final Object[] args = getArguments();
-        System.out.println("Hello " + a1.getName());
-        String sp = (String) args[0];
-        String bp = (String) args[1];
+        System.out.println("Hello " + getLocalName());
 
-        startPrice = Integer.parseInt(sp);
-        bottomPrice = Integer.parseInt(bp);
+        getAllBidders();
+        setStartAndBottomPrice();
+        informBidders();
 
-        System.out.println("Start price: " + startPrice);
-        System.out.println("Bottom price: " + bottomPrice);
+        addBehaviour(new TickerBehaviour(this, 1000) {
+            @Override
+            protected void onTick() {
+                receivedProposalMsgs();
+            }
+        });
+    }
 
-         /* find all HW2DistAI.Bidder agents */
+    protected void getAllBidders(){
         try {
-               /* subscribe Artifacts service by Curator agent */
+               /* Subscribe Artifacts service by Curator agent */
             DFAgentDescription dfd = new DFAgentDescription();
             ServiceDescription sd = new ServiceDescription();
 
@@ -47,16 +54,89 @@ public class Auctioneer extends Agent {
 
             for (int i = 0; i < bidderAgents.length; i++) {
                 bidderAgents[i] = result[i].getName();
-                System.out.println("bidders är: " + bidderAgents[i].toString());
+                System.out.println("The bidders are: " + bidderAgents[i].getName());
             }
         } catch (FIPAException e) {
             e.printStackTrace();
         }
-    }
 
+    }
+    protected void setStartAndBottomPrice(){
+        final Object[] args = getArguments();
+
+        String sp = (String) args[0];
+        String bp = (String) args[1];
+
+        startPrice = Integer.parseInt(sp);
+        bottomPrice = Integer.parseInt(bp);
+
+        System.out.println("Start price: " + startPrice);
+        System.out.println("Bottom price: " + bottomPrice);
+
+    }
+    protected void informBidders(){
+        ACLMessage inform = new ACLMessage(ACLMessage.INFORM);
+        for(int i = 0; i<bidderAgents.length; i++){
+            inform.addReceiver(bidderAgents[i]);
+        }
+        inform.setContent(String.valueOf(startPrice));
+        inform.setConversationId("The-auction");
+        send(inform);
+
+    }
+    protected void receivedProposalMsgs() {
+        receivedMsg = receive();
+        if(receivedMsg != null) {
+            if (receivedMsg.getPerformative() == 11) {
+
+
+                if(startPrice <= bottomPrice){
+                    System.out.println("Came to Bottom Price and ended the auction");
+
+                    return;
+
+                }
+
+                System.out.println(getLocalName() + " received " + receivedMsg.getContent() + " from a bidder");
+                if (proposeCount != bidderAgents.length) {
+                    if (receivedMsg.getContent().equalsIgnoreCase("accept")) {
+                        AID winner = receivedMsg.getSender();
+                        ACLMessage replyToWinner = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                        replyToWinner.addReceiver(winner);
+                        replyToWinner.setContent("won!!");
+                        send(replyToWinner);
+                        this.doDelete();
+
+                    }
+
+                    if (receivedMsg.getContent().equalsIgnoreCase("reject")) {
+                        proposeCount++;
+                    }
+                    if (proposeCount == bidderAgents.length) {
+                        proposeCount = 0;
+                        decreasePrice();
+                        startOver();
+                    }
+
+
+
+                }
+            }
+        }
+    }
 
     @Override
     protected void takeDown() {
+       // System.out.println("Shutting down now");
         super.takeDown();
     }
+
+    private void decreasePrice(){
+        startPrice = startPrice - (int)(startPrice * 0.10);
+    }
+    private void startOver(){
+        informBidders();
+    }
+
+
 }
--- src/HW2DistAI/Bidder.java
+++ src/HW2DistAI/Bidder.java
@@ -2,19 +2,38 @@ package HW2DistAI;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by araxi on 2016-11-17.
  */
 public class Bidder extends Agent {
 
-    private AID a1 = new AID("HW2DistAI.Bidder", AID.ISLOCALNAME);
+   //  private AID a1 = new AID("Bidder", AID.ISLOCALNAME);
+    int maxPrice = 0;
+    ACLMessage receivedMsg;
+    boolean x = true;
+
     @Override
     protected void setup() {
+        System.out.println("Hello " + getLocalName());
+        createServiceInDF();
+
+        addBehaviour(new TickerBehaviour(this, 1000) {
+            @Override
+            protected void onTick() {
+                receivedMsgs();
+            }
+        });
+    }
+
+    protected void createServiceInDF(){
 
         final DFAgentDescription dfd = new DFAgentDescription();
         dfd.setName(getAID());
@@ -28,7 +47,67 @@ public class Bidder extends Agent {
             e.printStackTrace();
         }
 
+    }
+
+    protected void receivedMsgs(){
+
+        receivedMsg = receive();
+        if(receivedMsg != null) {
+            getInformMsgs();
+
+            if(x == true) {
+                selectMaxPriceToPay();
+                x = false;
+            }
+
+            getAcceptedProposal();
+            sendProposeMsg();
+        }
+    }
+
+    protected  void getInformMsgs(){
+        if( receivedMsg.getPerformative()==7){
+            System.out.println("Item price is: " + receivedMsg.getContent());
+        }
+    }
+
+    protected void selectMaxPriceToPay(){
+        int temp = ThreadLocalRandom.current().nextInt(1, 100 +1);
+        int tempPrice = Integer.parseInt(receivedMsg.getContent());
+        maxPrice = (tempPrice / 100) * temp;
+        System.out.println(getLocalName() + "'s max price to pay for this item is: " + maxPrice);
 
     }
 
+    protected void getAcceptedProposal(){
+        if(receivedMsg.getPerformative() == 0){
+            System.out.println(getLocalName() + " " + receivedMsg.getContent());
+            this.doDelete();
+        }
+    }
+
+    protected void sendProposeMsg(){
+        ACLMessage proposeMsg = new ACLMessage(ACLMessage.PROPOSE);
+        int itemValue = 0;
+        try {
+            itemValue = Integer.parseInt(receivedMsg.getContent());
+        }catch (Exception e){
+            doDelete();
+        }
+        if(itemValue <= maxPrice){
+            proposeMsg.setContent("accept");
+        }
+        if(itemValue > maxPrice){
+            proposeMsg.setContent("reject");
+        }
+
+        AID auctioneer = receivedMsg.getSender();
+        proposeMsg.addReceiver(auctioneer);
+        send(proposeMsg);
+
+    }
+
+
 }
+
+
